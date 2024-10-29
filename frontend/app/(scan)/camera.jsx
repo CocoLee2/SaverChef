6ec21151692d7eef3f3088040c2e090381af7814
ngrinols1 <110038448+ngrinols1@users.noreply.github.com>
2024-10-29 13:36:58 -0700

    added camera information

--- frontend/app/(scan)/camera.jsx
+++ frontend/app/(scan)/camera.jsx
@@ -1,18 +1,19 @@
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { AppState, Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function camera() {
   const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('nothing scanned')
+
 
   if (!permission) {
-    // Camera permissions are still loading.
     return <View />;
   }
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
     return (
       <View style={styles.container}>
         <Text style={styles.message}>We need your permission to show the camera</Text>
@@ -21,17 +22,32 @@ export default function camera() {
     );
   }
 
+  const handleCodeScan = ({ type, data }) => {
+    // Call barcode API here
+    setScanned(true);
+    setText(data)
+    console.log('Type: ' + type + '\nData: ' + data)
+  };
+  
+
   function toggleCameraFacing() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <CameraView style={styles.camera} facing={facing} onBarcodeScanned={scanned ? undefined : handleCodeScan}
+      barcodeScannerSettings={{
+        barcodeTypes: ["upc_e", "upc_a"],
+      }}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
             <Text style={styles.text}>Flip Camera</Text>
+            <Text style={styles.text}>{text}</Text>
+
           </TouchableOpacity>
+        {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+
         </View>
       </CameraView>
     </View>
