a866f4ae725f134724f89de9a9d3d54bc31a4c22
Peter Evans <peter-evans@users.noreply.github.com>
2024-11-15 04:15:43 +0000

    Automated autopep8 fixes

--- backend/model/users.py
+++ backend/model/users.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from database.database import db
 from sqlalchemy.dialects.postgresql import JSON
 
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
--- backend/tests/api/fridge_test.py
+++ backend/tests/api/fridge_test.py
@@ -1,3 +1,3 @@
 def test_unknown_fridge(client):
-  response = client.get("/fridge/1000")
-  assert response.status_code == 400
+    response = client.get("/fridge/1000")
+    assert response.status_code == 400
