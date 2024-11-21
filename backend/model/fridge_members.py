b81ec9bff9aa998acadc3227484fa4f1d59ee2dd
Steven Tran <steven.tran077@gmail.com>
2024-11-21 12:35:05 -0800

    Add fridge member table

---
+++ backend/model/fridge_members.py
@@ -0,0 +1,16 @@
+from database.database import db
+from model.users import Users
+from model.fridge import Fridge
+
+
+class FridgeMembers(db.Model):
+    __tablename__ = 'fridge_members'
+    fridge_id = db.Column(db.Integer, db.ForeignKey(Fridge.id), nullable=False)
+    member_id = db.Column(db.Integer, db.ForeignKey(Users.id), nullable=False)
+
+    def __init__(self, fridge_id, member_id):
+        self.fridge_id = fridge_id
+        self.member_id = member_id
+
+    def serialize(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
